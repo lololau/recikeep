b07bea4d20b914f1d0cec4979a5138d6537a0316
Lauriane Verhille <verhille.lauriane@gmail.com>
2024-10-08 18:19:10 +0200

    fix: remove sentry

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,5 @@
 import { drizzle as drizzleTurso } from "drizzle-orm/libsql";
 import { type Config, createClient } from "@libsql/client";
-import * as Sentry from "@sentry/node";
-import { libsqlIntegration } from "sentry-integration-libsql-client";
 
 import * as schema from "./schema";
 
@@ -22,16 +20,5 @@ if (!process.env.CI_MODE && process.env.NODE_ENV === "production") {
 }
 const client = createClient(config);
 const db = drizzleTurso(client, { schema });
-
-Sentry.init({
-	dsn: "https://8ca7361b680e2e1d37c67865aef44df0@o4508087813996544.ingest.de.sentry.io/4508087823761488",
-	integrations: [
-		libsqlIntegration(client, Sentry, {
-			tracing: false,
-			breadcrumbs: false,
-			errors: false,
-		}),
-	],
-});
 
 export { db };
