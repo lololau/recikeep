4f9056103a6a7dae31b33da039e1c041c67d596c
Lauriane Verhille <verhille.lauriane@gmail.com>
2024-10-15 16:20:13 +0200

    feat: home recipes resize img + add placeholder

--- src/components/pages/HomePageRecipes.tsx
+++ src/components/pages/HomePageRecipes.tsx
@@ -4,33 +4,46 @@ import Link from "next/link";
 import { HiPencil } from "react-icons/hi";
 import type { RecipesFormated } from "recikeep/trpc/router/recipe";
 import { Tag } from "../Tag";
+import { MdImageNotSupported } from "react-icons/md";
 
 export function HomePageRecipe({ recipes }: { recipes: RecipesFormated[] }) {
 	return (
 		<>
 			{recipes.map((recipe, index) => {
 				return (
-					<Link href={`/recipe/${recipe.id}`} key={`${recipe.id}—${index}`}>
-						<li className="p-3 items-center border border-slate-300 rounded-lg hover:bg-gray-100">
-							<div className="flex flex-col">
+					<li
+						key={`${recipe.id}—${index}`}
+						className="p-3 items-center border border-slate-300 rounded-lg hover:bg-gray-100"
+					>
+						<Link href={`/recipe/${recipe.id}`}>
+							<div className="grid grid-cols-1 h-full content-between">
 								<div className="self-center">
-									{recipe.main_image && (
-										<div className="max-w-fit">
+									<div className="max-w-fit">
+										{recipe.main_image && (
 											<img
 												src={`https://utfs.io/f/${recipe.main_image}`}
 												alt="salade"
+												className="w-[144px] max-h-[191px] sm:w-[262px] sm:max-h-[350px]"
 											/>
-										</div>
-									)}
+										)}
+										{!recipe.main_image && (
+											<div className="w-[144px] h-[191px] sm:w-[262px] sm:h-[350px] bg-gray-200 flex flex-col items-center justify-center">
+												<p className="italic text-gray-800 text-sm sm:text-base">
+													Image manquante
+												</p>
+												<div className="text-3xl sm:text-6xl">
+													<MdImageNotSupported />
+												</div>
+											</div>
+										)}
+									</div>
 								</div>
 
-								<div className="flex flex-row items-center">
-									<div className="font-light text-gray-800 text-start grid flex-grow">
-										<p className="sm:text-base text-sm">{recipe.title}</p>
-										<p className="font-base text-gray-600 sm:text-sm text-xs">
-											Ref: {recipe.source}
-										</p>
-									</div>
+								<div className="flex flex-col text-start text-gray-800 font-light">
+									<p className="sm:text-base text-sm">{recipe.title}</p>
+									<p className="font-base text-gray-600 sm:text-sm text-xs">
+										Ref: {recipe.source}
+									</p>
 								</div>
 							</div>
 
@@ -48,8 +61,8 @@ export function HomePageRecipe({ recipes }: { recipes: RecipesFormated[] }) {
 									/>
 								))}
 							</div>
-						</li>
-					</Link>
+						</Link>
+					</li>
 				);
 			})}
 		</>
